0a1c3c931dc0bc92798003de7b614b97e711421c
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-07-31 03:13:50 -0400

    Repo init

---
+++ script/script.js
@@ -0,0 +1,118 @@
+const stickyTitle = document.querySelector('.sticky-title');
+const allProjectsBtn = document.querySelector('#AP');
+const beBtn = document.querySelector('#BE');
+const rpBtn = document.querySelector('#RP');
+const jsBtn = document.querySelector('#JSP');
+const lpBtn = document.querySelector('#LPP');
+const pyBtn = document.querySelector('#PYP');
+
+let allCards = document.querySelectorAll('.cards-box');
+let backendCards = document.querySelectorAll('.be-card');
+let reactCards = document.querySelectorAll('.rp-card');
+let jsCards = document.querySelectorAll('.js-card');
+let lpCards = document.querySelectorAll('.lp-card');
+let pythonCards = document.querySelectorAll('.py-card');
+
+let cardTypes = {
+    backend: backendCards,
+    react: reactCards,
+    js: jsCards,
+    landing: lpCards,
+    python: pythonCards,
+    all: allCards
+};
+
+const projectsTop = document.querySelector('.projects-section');
+
+function filterDisplay(type) {
+    if (type !== "all") {
+        Object.values(cardTypes).forEach((cards) => {
+            cards.forEach((card) => {
+                card.style.display = "none";
+            });
+        });
+    };
+
+    if (cardTypes[type]) {
+        cardTypes[type].forEach((card) => {
+            card.style.display = "flex";
+        });
+    }
+    projectsTop.scrollIntoView({ behavior: "smooth" });
+};
+
+beBtn.addEventListener('click', () => {
+    filterDisplay("backend");
+});
+rpBtn.addEventListener('click', () => {
+    filterDisplay("react");
+});
+jsBtn.addEventListener('click', () => {
+    filterDisplay("js");
+});
+
+
+// Mobile Scroll Animation // 
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.cards-box');
+    const projectsSection = document.querySelector('.projects-section');
+    const offset = 300;
+
+    function onScroll() {
+        const sectionTop = projectsSection.getBoundingClientRect().top;
+        const sectionBottom = projectsSection.getBoundingClientRect().bottom;
+
+        cards.forEach(card => {
+            const cardTop = card.getBoundingClientRect().top;
+            const cardBottom = card.getBoundingClientRect().bottom;
+            if (cardTop < window.innerHeight - offset && cardBottom > 400) {
+                card.classList.add('visible');
+            } else {
+                card.classList.remove('visible');
+            }
+        });
+    };
+    window.addEventListener('scroll', onScroll);
+    onScroll();
+});
+// Projects Counter // 
+const proCount = document.createElement('p');
+proCount.innerText = `(${allCards.length})`
+proCount.style = "color:white; font-size: .7rem; position:absolute; top: 25%; right: 25px;";
+stickyTitle.insertBefore(proCount, stickyTitle.children[1]);
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector('.animated-typing');
+    const words = ["Wedding Ceremony.", "Custom Event.", "Bar / Bat Mitzvah.", "Hina Ceremony.", "Catering Menu."];
+
+    let currentWordIndex = 0;
+    const animateText = (textToAnimate) => {
+        container.innerHTML = '';
+
+        for (let i = 0; i < textToAnimate.length; i++) {
+            const letter = textToAnimate[i];
+            const span = document.createElement('span');
+            span.textContent = letter;
+            span.style.animationDelay = `${i * 0.1}s`;
+            container.appendChild(span);
+        }
+    };
+
+    const startAnimation = () => {
+        const word = words[currentWordIndex];
+
+        animateText(word);
+        currentWordIndex = (currentWordIndex + 1) % words.length;
+        setTimeout(() => {
+            startAnimation();
+        }, word.length * 100 + 2000);
+    };
+    startAnimation();
+});
+
+
+
+
+
