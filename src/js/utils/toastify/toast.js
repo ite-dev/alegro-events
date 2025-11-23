0c52ac8692f5948243f11f2c23248cef5686deba
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-11-23 09:06:39 -0500

    test - toastified.me

--- src/js/utils/toastify/toast.js
+++ src/js/utils/toastify/toast.js
@@ -1,10 +1,6 @@
 import Toastify from "toastify-js";
 import "./css/toast.css";
 
-const toastifyPolicy = window.trustedTypes?.createPolicy('toastifyPolicy', {
-    createHTML: (input) => input,
-});
-
 const defaultOptions = {
     duration: 3000,
     gravity: "top",
@@ -13,15 +9,22 @@ const defaultOptions = {
     close: true,
 };
 
+/**
+ * Create a Trusted Types / CSP-compliant toast
+ * @param {string} msg - Message to display
+ * @param {string} className - CSS class for styling
+ * @param {number} duration - Duration of the toast in ms (-1 for persistent)
+ * @returns {object} toast instance
+ */
 function createToast(msg, className = "", duration = defaultOptions.duration) {
     const node = document.createElement("div");
     node.style.color = "#E7D8BA";
 
-    if (toastifyPolicy) {
-        node.innerHTML = toastifyPolicy.createHTML(msg);
+    if (window.toastifyPolicy) {
+        node.innerHTML = window.toastifyPolicy.createHTML(msg);
     } else {
         node.textContent = msg;
-    }
+    };
 
     const toast = Toastify({
         ...defaultOptions,
@@ -33,7 +36,7 @@ function createToast(msg, className = "", duration = defaultOptions.duration) {
 
     toast.showToast();
     return toast;
-}
+};
 
 export const successMsg = (msg) => createToast(msg, "success-toast", 10000);
 export const errorMsg = (msg) => createToast(msg, "error-toast", 5000);
