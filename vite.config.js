4383a76580eeba5900ef706c523d5991fc6d4db4
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-11-29 12:30:33 -0500

    test - not found page

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import FullReload from 'vite-plugin-full-reload';
-import { resolve } from 'path';
 
 export default defineConfig({
     root: './',
     plugins: [
-        FullReload(['public/css/**/*.css']),
+        FullReload(['public/css/**/*.css'])
     ],
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                gallery: resolve(__dirname, 'pages/gallery.html'),
-                weddings: resolve(__dirname, 'pages/weddings.html'),
-                contactUs: resolve(__dirname, 'pages/contact-us.html'),
-                '404': resolve(__dirname, 'pages/404.html'),
+                main: 'index.html',
+                gallery: 'pages/gallery.html',
+                weddings: 'pages/weddings.html',
+                contactUs: 'pages/contact-us.html',
+                '404': '404.html',
             },
         },
-        outDir: 'dist',
     },
     base: '/',
 });
+
